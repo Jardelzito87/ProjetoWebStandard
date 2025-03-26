e53c619312460ffe9705450a6ec93a0c14f0774c
JARDEL SOUSA GOMES <jardel.sgomes1@senacsp.edu.br>
2025-03-26 20:08:07 -0300

    buscaCep

--- js/buscacep.js
+++ js/buscacep.js
@@ -1,3 +1,7 @@
+
+const urlViaCep = "https://viacep.com.br/ws/[[CEP]]/json/";
+ 
+
 function limparCep() {
     document.getElementsByName("BuscarCep")[0].value = "";
     document.getElementsByName("endereco")[0].value = "";
@@ -11,5 +15,41 @@ function limparCep() {
 }
 
 function BuscarCep() {
+    const requestOptions = {
+        method: "GET",
+        redirect: "follow"
+    };
 
+    let cep = document.getElementsByClassName("BuscarCep")[0].value;
+
+    let url = urlViaCep.replace("[[cep]]", cep);
+
+    fetch(url,requestOptions)
+         .then((response) =>{
+            if(response.ok){
+                return response.json(); //convertendo o a resposta em JSON
+            }
+
+            throw new Error(`ERRO: HTTP ${reponse.status}`);
+
+         })
+         .then((result) =>{
+            document.getElementsByName("endereco")[0].value = result.logradouro || "";
+            document.getElementsByName("complemento")[0].value = result.complemento || "";
+            document.getElementsByName("bairro")[0].value = result.bairro || "";
+            document.getElementsByName("cidade")[0].value = result.localidade || "";
+            document.getElementsByName("estado")[0].value = result.uf || "";   
+            
+            document.getElementsByName("endereco")[0].disabled = true
+            document.getElementsByName("complemento")[0].disabled = true
+            document.getElementsByName("bairro")[0].disabled = true
+            document.getElementsByName("cidade")[0].disabled = true
+            document.getElementsByName("estado")[0].disabled = true
+
+            document.getElementsByName("numero")[0].focus();
+
+         })
+         .catch((erro) =>{
+            alert(`ERRO ao buscar o cep\n${erro}`)
+         });
 }
