df1b005f9a506ff6d28e2b0e0b2fb9cfcef83df6
JARDEL SOUSA GOMES <jardel.sgomes1@senacsp.edu.br>
2025-03-26 22:08:03 -0300

    atualização de cep

--- js/buscacep.js
+++ js/buscacep.js
@@ -20,9 +20,10 @@ function BuscarCep() {
         redirect: "follow"
     };
 
-    let cep = document.getElementsByClassName("BuscarCep")[0].value;
+    let cep = document.getElementsByName("BuscarCep")[0].value;
 
-    let url = urlViaCep.replace("[[cep]]", cep);
+    let url = urlViaCep.replace("[[CEP]]", cep);
+
 
     fetch(url,requestOptions)
          .then((response) =>{
@@ -30,7 +31,8 @@ function BuscarCep() {
                 return response.json(); //convertendo o a resposta em JSON
             }
 
-            throw new Error(`ERRO: HTTP ${reponse.status}`);
+            throw new Error(`ERRO: HTTP ${response.status}`);
+
 
          })
          .then((result) =>{
@@ -52,4 +54,41 @@ function BuscarCep() {
          .catch((erro) =>{
             alert(`ERRO ao buscar o cep\n${erro}`)
          });
+}
+
+function BuscarCEPFecth(){
+
+}
+
+function BuscarCEP(){
+
+    let cep = document.getElementsByName("BuscaCep")[0].value;
+    let url = urlViaCep.replace("[[cep]]", cep);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState === 4 || xhr.status === 200){
+            const result = JSON.parse(xhr.responseText); // converter JSON
+            
+            document.getElementsByName("endereco")[0].value = result.logradouro || "";
+            document.getElementsByName("complemento")[0].value = result.complemento || "";
+            document.getElementsByName("bairro")[0].value = result.bairro || "";
+            document.getElementsByName("cidade")[0].value = result.localidade || "";
+            document.getElementsByName("estado")[0].value = result.uf || "";   
+            
+            document.getElementsByName("endereco")[0].disabled = true
+            document.getElementsByName("complemento")[0].disabled = true
+            document.getElementsByName("bairro")[0].disabled = true
+            document.getElementsByName("cidade")[0].disabled = true
+            document.getElementsByName("estado")[0].disabled = true
+
+            document.getElementsByName("numero")[0].focus();
+
+        } else {
+            alert("ERRO ao buscar CEP. Status HTTP: ", xhr.status);
+        }
+    }
+
 }
